3c082852264667b71b1a5ff4fde265db32d41b32
Bryan Paxton <starbelly@pobox.com>
2018-06-30 05:00:17 -0500

    Re-add trailing / to urls, regex it out of name in link

--- src/_plugins/generator.rb
+++ src/_plugins/generator.rb
@@ -1,5 +1,18 @@
 require "json"
 
+module Jekyll
+  module RegexFilter
+    def replace_regex(input, reg_str, repl_str)
+      re = Regexp.new reg_str
+
+      # This will be returned
+      input.gsub re, repl_str
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::RegexFilter)
+
 class CardGenerator < Jekyll::Generator
 
   def generate(site)
